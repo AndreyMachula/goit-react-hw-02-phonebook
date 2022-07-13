b95ea9fbeb80caf988fd0a0666ece1624e3d6f5a
AndreyMachula <amcz2020@gmail.com>
2022-07-14 01:13:15 +0200

    update

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -40,7 +40,7 @@ class InputForm extends Component {
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          placeholder="+38 (099) 999-99-99"
+          placeholder="+38 (099)"
           value={this.state.number}
           required
           onChange={this.handleInput}
